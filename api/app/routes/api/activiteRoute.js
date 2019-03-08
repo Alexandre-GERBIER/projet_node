3d7d0d7df52b88a7f67fb90531f5655af8621827
Alexandre Gerbier <alexandre.gerbier@etu.univ-nantes.fr>
2019-03-08 11:53:57 +0100

    activite route

---
+++ api/app/routes/api/activiteRoute.js
@@ -0,0 +1,28 @@
+/* Load Modules */
+const express = require('express');
+const router = express.Router();
+
+/* Load controller */
+const ActiviteController = require('../../controller/activiteController');
+const activiteController = new ActiviteController();
+
+/**
+ * Activite Entity routes
+ */
+router.get('/count', function (req, res) {
+    activiteController.countAll(res);
+});
+
+router.get('/exists/:id', function (req, res) {
+    activiteController.exists(req, res);
+});
+
+router.get('/:id', function (req, res) {
+    activiteController.findById(req, res);
+});
+
+router.get('/', function (req, res) {
+    activiteController.findAll(res);
+});
+
+module.exports = router;
