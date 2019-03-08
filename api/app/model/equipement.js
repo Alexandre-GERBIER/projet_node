9f7bedd415a5a8e16497205f83e4a3c74902041d
swanroyer <swan.royer@gmail.com>
2019-03-08 12:16:17 +0100

    Equipement

---
+++ api/app/model/equipement.js
@@ -0,0 +1,14 @@
+
+class Equipement {
+    constructor(noDeLInstallation,noDeLEquipement,nomEquipement,nbrEquipement,nbrPlaceTribune,natureDeLEquipement,nbrVestiaire) {
+      this.noDeLInstallation = noDeLInstallation;
+      this.noDeLEquipement = noDeLEquipement;
+      this.nomEquipement = nomEquipement;
+      this.nbrEquipement = nbrEquipement;
+      this.nbrPlaceTribune = nbrPlaceTribune;
+      this.natureDeLEquipement = natureDeLEquipement;
+      this.nbrVestiaire = nbrVestiaire;
+    }
+}
+
+module.exports = Equipement;
