1a2b7e6b735b39b2f6532aeba58aab2e4f2f273c
swanroyer <swan.royer@gmail.com>
2019-03-08 11:18:45 +0100

    instalation

---
+++ api/app/model/Installation.js
@@ -0,0 +1,17 @@
+
+class Installation {
+    constructor(noDeLIntallation,nomUsuelDeLIstallation,codePostal,nomDeLaCommune,numDeLaVoie,nomDeLaVoie,nomDuLieuDit,installationPariculiere,nbrplaceparking,dateCreation) {
+        this.noDeLIntallation = noDeLIntallation;
+        this.nomUsuelDeLIstallation = nomUsuelDeLIstallation;
+        this.codePostal = codePostal;
+        this.nomDeLaCommune = nomDeLaCommune;
+        this.numDeLaVoie = numDeLaVoie;
+        this.nomDeLaVoie = nomDeLaVoie;
+        this.nomDuLieuDit = nomDuLieuDit;
+        this.installationPariculiere = installationPariculiere;
+        this.nbrplaceparking = nbrplaceparking;
+        this.dateCreation = dateCreation;
+    }
+}
+
+module.exports = Installation;
