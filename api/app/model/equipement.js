67e4e406243c9cbf851428f45ec222724b5f5c51
swan <swan.royer@etu.univ-nantes.fr>
2019-03-09 15:00:05 +0100

    Equipementmisajour

--- api/app/model/equipement.js
+++ api/app/model/equipement.js
@@ -1,9 +1,10 @@
 
 class Equipement {
-    constructor(noDeLInstallation,noDeLEquipement,nomEquipement,nbrEquipement,nbrPlaceTribune,natureDeLEquipement,nbrVestiaire) {
+    constructor(noDeLInstallation,noDeLEquipement,nomEquipement,typeDeEquipement,nbrEquipement,nbrPlaceTribune,natureDeLEquipement,nbrVestiaire) {
       this.noDeLInstallation = noDeLInstallation;
       this.noDeLEquipement = noDeLEquipement;
       this.nomEquipement = nomEquipement;
+      this.typeDeEquipement = typeDeEquipement;
       this.nbrEquipement = nbrEquipement;
       this.nbrPlaceTribune = nbrPlaceTribune;
       this.natureDeLEquipement = natureDeLEquipement;
