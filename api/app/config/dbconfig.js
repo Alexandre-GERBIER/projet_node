dec1516b7414a527d2b8538f66de4115e2c2e4c3
Alexandre Gerbier <alexandre.gerbier@etu.univ-nantes.fr>
2019-03-15 11:06:52 +0100

    creation Bd + debut population

--- api/app/config/dbconfig.js
+++ api/app/config/dbconfig.js
@@ -1,5 +1,11 @@
 /* Load modules */
 let sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+const csv = require('csv');
+const parse = require('csv-parse');
+const path = require('path');
+//TODO foreign keys + check type : text vs int
+// TODO v2 : localisation pour géoloc + distance
 
 /*
  * Database configuration
@@ -22,7 +28,7 @@ const createActivite = function () {
             "Dans_salle_specialisable TEXT NOT NULL," +
             "Niveau_de_lActivite TEXT NOT NULL," +
             "localisation TEXT NOT NULL" +
-            "PRIMARY KEY (activite_code, numero_de_la_fiche_equipement)," ;
+            "PRIMARY KEY (Activite_code, numero_de_la_fiche_equipement));" ;
          //   "FOREIGN KEY (numero_de_la_fiche_equipement) REFERENCES equipement(numero_de_la_fiche_equipement))";
         db.run(sqlRequest, [], (err) => {
 
@@ -39,9 +45,238 @@ const createActivite = function () {
     });
 };
 
+const createEquipement = function() {
+    return new Promise(function (resolve, reject) {
+        const sqlRequest = "CREATE TABLE IF NOT EXISTS equipement (" +
+            "noDeLInstallation TEXT NOT NULL," +
+            "noDeLEquipement TEXT NOT NULL," +
+            "nomEquipement TEXT NOT NULL," +
+            "nbrEquipement INT NOT NULL," +
+            "nbrPlaceTribune INT NOT NULL," +
+            "natureDeLEquipement TEXT NOT NULL," +
+            "nbrVestiaire INT NOT NULL," +
+            "PRIMARY KEY (numero_de_la_fiche_equipement));" +
+            //"FOREIGN KEY (numero_de_l_installation) REFERENCES installation(numero_de_l_installation))";
+
+        db.run(sqlRequest, [], (err) => {
+
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                console.log("Equipement créée");
+                resolve(this);
+            }
+
+        });
+
+    });
+};
+
+const createInstallation = function() {
+    return new Promise(function (resolve, reject) {
+        const sqlRequest = "CREATE TABLE IF NOT EXISTS installation (" +
+            "departement TEXT NOT NULL," +
+            "noDeLInstallation TEXT NOT NULL, " +
+            "nomUsuelDeLInstallation TEXT NOT NULL, " +
+            "codePostal TEXT NOT NULL, " +
+            "nomDeLaCommune TEXT NOT NULL, " +
+            "numDeLaVoie TEXT NOT NULL," +
+            "nomDeLaVoie TEXT NOT NULL," +
+            "nomDuLieuDit TEXT NOT NULL," +
+            "installationParticuliere TEXT NOT NULL," +
+            "nbrplaceparking TEXT NOT NULL," +
+            "dateCreation TEXT NOT NULL," +
+            "PRIMARY KEY (numero_de_l_installation))";
+
+        db.run(sqlRequest,[], (err) => {
+
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                console.log("Installation créée");
+                resolve(this);
+            }
+        });
+
+    })
+};
+
+const populateInstallation =  function() {
+    return new Promise(function (resolve, reject) {
+        const fileName = 'data/234400034_004-010_fiches-installations-rpdl_extra_small.csv';
+        const stream = fs.createReadStream(fileName, {encoding: 'utf8'});
 
 
+        const parser = parse({
+            delimiter: ';',
+            columns: header =>
+                header.map( column => column.normalize('NFD').
+                replace(/[\u0300-\u036f]/g, "").
+                replace(/[^a-z0-9]/gmi, "_").
+                replace(/\s+/g, '_').
+                toLowerCase())
+        });
 
+
+        parser.on('readable', function () {
+            let row;
+
+            while (row = this.read()) {
+                //TODO changer les parametres en fonction de ceux choisi
+                const sqlRequest = "INSERT OR IGNORE into installation (numero_de_l_installation, nom_usuel_de_l_installation, code_postal, nom_de_la_commune) " +
+                    "VALUES ($dept,$noDeLInstallation, $nomUsuelDeLInstallation, $codePostal, $nomDeLaCommune, $numVoie, $nomVoie, $nomLieuDit, $instPart, $nbrPark, $dateCreation)";
+                const sqlParams = {
+                    $dept: String(row.code_du_departement),
+                    $noDeLInstallation: String(row.numero_de_l_installation),
+                    $nomUsuelDeLInstallation: row.nom_usuel_de_l_installation,
+                    $codePostal: String(row.code_postal),
+                    $nomDeLaCommune: String(row.nom_de_la_commune),
+                    $numVoie: String(row.numero_de_la_voie),
+                    $nomVoie: row.nom_de_la_voie,
+                    $nomLieuDit: String(row.nom_du_lieu_dit),
+                    $instPart: String(row.installation_particuliere),
+                    $nbrPark: String(row.nomber_total_de_place_de_parking),
+                    $dateCreation: String(row.)
+                    // TODO quel date ? màj fiche ou création fiche
+
+                };
+
+
+                db.run(sqlRequest, sqlParams, function (err) {
+                    if (err)
+                        console.log(err);
+                });
+
+            }
+        });
+
+
+        stream.pipe(parser);
+
+        parser.on('finish', function ()  {
+            console.log("Installations importées");
+            resolve(this);
+
+        });
+
+        parser.on("error", (err) =>{
+            console.log(err);
+            reject(err);
+        });
+
+    })
+}
+
+const populateEquipement =  function() {
+    return new Promise(function (resolve, reject) {
+        const fileName = 'data/234400034_004-011_fiches-equipements-rpdl_extra_small.csv';
+        const stream = fs.createReadStream(fileName, {encoding: 'utf8'});
+
+        const parser = parse({
+            delimiter: ';',
+            columns: header =>
+                header.map( column => column.normalize('NFD').
+                replace(/[\u0300-\u036f]/g, "").
+                replace(/[^a-z0-9]/gmi, "_").
+                replace(/\s+/g, '_').
+                toLowerCase())
+        });
+
+        parser.on('readable', function () {
+            let row;
+
+            while (row = this.read()) {
+                const sqlRequest = "INSERT OR IGNORE into equipement (noDeLInstallation,noDeLEquipement,nomEquipement,typeDeEquipement,nbrEquipement,nbrPlaceTribune,natureDeLEquipement,nbrVestiaire) " +
+                    "VALUES ($noDeLInstallation,$noDeLEquipement,$nomEquipement,$typeDeEquipement,$nbrEquipement,$nbrPlaceTribune,$natureDeLEquipement,$nbrVestiaire)";
+                const sqlParams = {
+                    $numeroDeLaFicheEquipement: row.numero_de_la_fiche_equipement,
+                    $numeroDeLInstallation: row.numero_de_l_installation
+
+
+                };
+
+                db.run(sqlRequest, sqlParams, function (err) {
+                    if (err)
+                        console.log(err);
+                });
+            }
+        });
+
+
+        stream.pipe(parser);
+
+        parser.on('finish', function ()  {
+            console.log("Equipements importés");
+            resolve(this);
+
+        });
+
+        parser.on("error", (err) =>{
+            console.log(err);
+            reject(err);
+        });
+
+    })
+}
+
+const populateActivite =  function() {
+    return new Promise(function (resolve, reject) {
+        //TODO changer les pramaetres en fonction de ceux choisi
+        const fileName = 'data/234400034_004-009_activites-des-fiches-equipements-rpdl_extra_small.csv';
+        const stream = fs.createReadStream(fileName, {encoding: 'utf8'});
+
+        const parser = parse({
+            delimiter: ';',
+            columns: header =>
+                header.map( column => column.normalize('NFD').
+                replace(/[\u0300-\u036f]/g, "").
+                replace(/[^a-z0-9]/gmi, "_").
+                replace(/\s+/g, '_').
+                toLowerCase())
+        });
+
+        parser.on('readable', function () {
+            let row;
+
+            while (row = this.read()) {
+                const sqlRequest = "INSERT OR IGNORE into activite(activite_code, activite_libelle, numero_de_la_fiche_equipement) " +
+                    "VALUES ($activiteCode, $activiteLibelle, $numeroDeLaFicheEquipement)";
+                const sqlParams = {
+                    $activiteCode: row.activite_code,
+                    $activiteLibelle: row.activite_libelle,
+                    $numeroDeLaFicheEquipement : row.numero_de_la_fiche_equipement
+                };
+
+
+                db.run(sqlRequest, sqlParams, function (err) {
+                    if (err) {
+                        console.log(err);
+                        console.log(sqlRequest, sqlParams.$activiteCode, sqlParams.$activiteLibelle, sqlParams.$numeroDeLaFicheEquipement);
+                    }
+                });
+            }
+        });
+
+
+        stream.pipe(parser);
+
+        parser.on('finish', function ()  {
+            console.log("Activités importées");
+            resolve(this);
+
+        });
+
+        parser.on("error", (err) =>{
+            console.log(err);
+            reject(err);
+        });
+
+    })
+}
 
 /* Init car and driver tables if they don't exist */
 let init = function () {
--- vueJs/src/components/HelloWorld.vue
+++ vueJs/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1> ceci est un test </h1>
     <h2>Essential Links</h2>
     <ul>
       <li>
